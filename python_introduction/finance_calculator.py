6a8ea5cd741620ff8fd205ecc8ffd071f2bce54c
Youssef Waleed <yoyo.kotlin@gmail.com>
2024-12-15 01:52:40 +0200

    FINAL 2 COMMIT

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,6 @@
-income = int(input("Enter your monthly income: "))
-expenses = int(input("Enter your total monthly expenses: "))
-savings = income - expenses
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
+savings = monthly_income - monthly_expenses
 projected_savings = int(savings * 12 + (savings * 12 * 0.05))
 
 print(f'Your monthly savings are ${savings}.')
