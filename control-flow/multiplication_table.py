9fbe73b882edc4c14bf2f1640dfee8e4e4057f1b
Youssef Waleed <yoyo.kotlin@gmail.com>
2024-12-24 01:28:15 +0200

    multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 number = input("Enter a number to see its multiplication table:")
 
-for i in range(10):
-    print(f'{number} * {i+1} = {int(number)*(i+1)}')
+for i in range(1,11):
+    print(f'{number} * {i} = {int(number)*i}')
 
