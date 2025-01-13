ee4b7e2e9fabf4c8cca44b69de53da166e4cf12e
Youssef Waleed <yoyo.kotlin@gmail.com>
2025-01-13 22:32:44 +0200

    ADDED FNS and DSA

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,14 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1, num2, operation):
     if operation == "add":
-        result = num1 + num2
-        return result
+        return num1 + num2
     elif operation == "subtract":
-        result = num1 - num2
-        return result
+        return num1 - num2
     elif operation == "multiply":
-        result = num1 * num2
-        return result
+        return num1 * num2
     elif operation == "divide":
         if num2 == 0:
-            print("CAN'T DIVIDE BY ZERO!!")
+            return "CAN'T DIVIDE BY ZERO!!"
         else:
-            result = num1 / num2
-            return result
-    
+            return num1 / num2
+    else:
+        return "Invalid operation"
