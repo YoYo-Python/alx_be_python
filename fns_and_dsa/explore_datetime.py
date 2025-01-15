3969b3bfad2d1db579ee9f9e6cafe7fa4ca544a3
Youssef Waleed <yoyo.kotlin@gmail.com>
2025-01-15 21:15:38 +0200

    Added Datetime shit

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+import datetime
+
+
+now = datetime.datetime.now()
+def display_current_datetime():
+    current_date = now.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time: "+ current_date)
+    
+    def calculate_future_date():
+        wanted_days = int(input("Enter the number of days to add to the current date: "))
+        future_date = now + datetime.timedelta(days=wanted_days)
+        future_date = future_date.strftime("%Y-%m-%d")
+        print(f'Future date is: {future_date}')
+    calculate_future_date()
+
+
+display_current_datetime()
+
+
